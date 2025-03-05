ded6cc4f697b58e63c904e13f7ff80195d1c996b
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-04 22:28:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 9; i += 2) 
+        {
+            int jInicio = i + 6; 
+            for (int j = jInicio; j >= jInicio - 2; j--) 
+            {
+                Console.WriteLine($"I={i} J={j}");
+            }
+        }
+    }
+}
+
